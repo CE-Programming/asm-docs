bce27355a549fc69b2ec126599945dac7455af0f
Adrien Bertrand <bertrand.adrien@gmail.com>
2015-12-11 04:35:26 -0500

    "Link" to select content of code blocks

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,24 @@
     }
   }
 })();
+
+
+/*****************************************************************************/
+// Utils
+/*****************************************************************************/
+
+function selectElementContents(el) {
+    if (window.getSelection && document.createRange) {
+        // IE 9 and non-IE
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (document.body.createTextRange) {
+        // IE < 9
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.select();
+    }
+}
